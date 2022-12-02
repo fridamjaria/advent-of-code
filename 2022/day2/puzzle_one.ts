3c5cd11cc93e00fa7020b384f9a4525d1c71797c
Frida Mjaria <fmjaria@gmail.com>
2022-12-02 09:18:28 +0200

    Day two Puzzle one

---
+++ 2022/day2/puzzle_one.ts
@@ -0,0 +1,48 @@
+import { readFileSync } from "fs";
+
+enum ShapeScore {
+  Rock = 1,
+  Paper,
+  Scissor
+};
+
+enum RoundScore {
+  Loss = 0,
+  Draw = 3,
+  Win = 6
+};
+
+const strategyMap: { [x: string]: number } = {
+  X: ShapeScore.Rock,
+  Y: ShapeScore.Paper,
+  Z: ShapeScore.Scissor
+}
+
+const opponentStrategyMap: { [x: string]: number } = {
+  A: ShapeScore.Rock,
+  B: ShapeScore.Paper,
+  C: ShapeScore.Scissor,
+};
+
+const isWinningMove = (score: number, opponentScore: number): boolean => {
+  return score === ShapeScore.Rock && opponentScore === ShapeScore.Scissor ||
+    score === ShapeScore.Scissor && opponentScore === ShapeScore.Paper ||
+    score === ShapeScore.Paper && opponentScore === ShapeScore.Rock;
+}
+
+const fileContents = readFileSync('input.txt', 'utf-8');
+let totalScore = 0;
+fileContents.split('\r\n').forEach(pair => {
+  const playedShapes = pair.split(' ');
+  const opponentShapeScore = opponentStrategyMap[playedShapes[0]];
+  const shapeScore = strategyMap[playedShapes[1]];
+
+  totalScore += shapeScore;
+  if (opponentShapeScore === shapeScore) {
+    totalScore += RoundScore.Draw;
+  } else if (isWinningMove(shapeScore, opponentShapeScore)) {
+    totalScore += RoundScore.Win;
+  }
+});
+
+console.log(`Total score: ${totalScore}`);
